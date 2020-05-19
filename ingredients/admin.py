d537598162b5afd8c6287c939f4bc7760a101a84
Vivian Machindano <vivianmachindano@Vivians-MacBook-Pro.local>
2020-05-19 10:25:32 +0200

    added models to admin.py

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Ingredients, Recipe, Part, Steps, Unit, Author
+
+
+
+
+
+class StepsInLine(admin.TabularInline):
+   model = Steps
+   
+
+
+
+class PartInLine(admin.TabularInline):
+   model = Part
+   
+
+
+
+class IngredientsInLine(admin.TabularInline):
+   model = Ingredients
+   
+
+
+
+
+
+class AdminPart(admin.ModelAdmin):
+	inlines=[IngredientsInLine, StepsInLine]
+
+
+
+
+
+
+
+
+
+
+class AdminRecipe(admin.ModelAdmin):
+	inlines=[PartInLine ]
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+admin.site.register(Recipe, AdminRecipe)
+
+admin.site.register(Part, AdminPart)
+admin.site.register(Category)
+admin.site.register(Unit)
+admin.site.register(Author)
+
+
